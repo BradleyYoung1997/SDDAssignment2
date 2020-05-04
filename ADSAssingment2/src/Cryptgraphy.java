5c7dfd8b8061fda5a1551955aba707753435fed7
Bradey <Bradey@DESKTOP-IA7I1JV>
2020-05-04 11:26:32 +1000

    Client,sever, and encryption classes

---
+++ ADSAssingment2/src/Cryptgraphy.java
@@ -0,0 +1,9 @@
+/*
+ * Author: Bradley Young 12110283
+ * Date: 04/05/2020
+ * Purpose: 
+ */
+public class Cryptgraphy 
+{
+
+}
---
+++ ADSAssingment2/src/TCPServer.java
@@ -0,0 +1,24 @@
+
+import java.net.Socket;
+
+/*
+ * Author: Bradley Young 12110283
+ * Date: 04/05/2020
+ * Purpose: Server/Connection classes for the system.
+ */
+public class TCPServer 
+{
+
+    static int portNumber = 1808;
+    static Socket clientSocket;
+    public static void main(String[] args) 
+    {
+        
+    }
+
+}
+
+class Conncection
+{
+    
+}
