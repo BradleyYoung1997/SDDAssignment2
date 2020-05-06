cedd1e151f74d672d0b2c900f636a92c03f01271
Bradey <Bradey@DESKTOP-IA7I1JV>
2020-05-06 15:55:25 +1000

    SDD Assignment

--- TransportDBSystem/src/Vehicle.java
+++ TransportDBSystem/src/Vehicle.java
@@ -5,7 +5,45 @@
  */
 public class Vehicle 
 {
-    String numPlate;
-    String model;
-    int year;
+    private String numPlate;
+    private String model;
+    private int year;
+    private String owner;
+    private String address;
+    private long phone;
+    
+    public Vehicle(String numPlate, String model, int year, String owner, 
+                                                     String address, long phone)
+    {
+       this.numPlate = numPlate;
+       this.model = model;
+       this.year = year;
+       this.owner = owner;
+       this.address = address;
+       this.phone = phone;
+    }
+        public void setNumPlate(String numplate)
+        {
+            this.numPlate = numPlate;
+        }
+        public void setModel(String model)
+        {
+            this.model = model; 
+        }
+        public void setYear(int year)
+        {
+            this.year = year;
+        }
+        public void setOwner(String owner)
+        {
+            this.owner = owner;
+        }
+        public void setAddress(String address)
+        {
+            this.address = address;
+        }
+        public void setPhone(long phone)
+        {
+            this.phone = phone;
+        }
 }
